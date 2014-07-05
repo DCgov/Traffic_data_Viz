e737f72119463f9749afd2a8290c922b036aaa27
Kevin Lu <kevyclvt@gmail.com>
2014-07-05 18:27:43 -0400

    update database query and web cgi entry

---
+++ pycode/datavizcgi.py
@@ -0,0 +1,44 @@
+__author__ = 'Kevin'
+
+import db_access
+import cgi, cgitb
+
+'''
+example:
+http://localhost:10808/dataviz/datavizcgi.py?method=query_by_corridor_group&corridor_id=1&start_date=2013-10-01&end_date=2013-10-31
+http://localhost:10808/dataviz/datavizcgi.py?method=query_by_acisa&acisa=2135&start_date=2013-10-01&end_date=2013-10-31
+
+'''
+
+print "Status: 200 OK"
+print "Content-Type: text/plain;charset=utf-8"
+print
+
+# Create instance of FieldStorage
+form = cgi.FieldStorage()
+
+# Get data from fields
+method = form.getvalue('method')
+
+if method == 'query_by_acisa':
+    acisa = form.getvalue('acisa')
+    start_date = form.getvalue('start_date')
+    end_date = form.getvalue('end_date')
+    output_format = form.getvalue('output_format')
+    if output_format is None:
+        print db_access.query_by_acisa(acisa, start_date, end_date)
+    else:
+        print db_access.query_by_acisa(acisa, start_date, end_date, output_format)
+
+elif method == 'query_by_corridor_group':
+    corridor_id = form.getvalue('corridor_id')
+    start_date = form.getvalue('start_date')
+    end_date = form.getvalue('end_date')
+    output_format = form.getvalue('output_format')
+    if output_format is None:
+        print db_access.query_by_corridor_group(corridor_id, start_date, end_date)
+    else:
+        print db_access.query_by_corridor_group(corridor_id, start_date, end_date, output_format)
+
+else:
+    print 'No request defined'
--- pycode/db_access.py
+++ pycode/db_access.py
@@ -9,10 +9,14 @@ Description: Database method assembly for traffic visualization
 import pyodbc
 import xml.etree.cElementTree as ET
 import datetime
+import os
 
 # Hard coding area
 CorridorFile = 'corridors.txt'
-db_setting_path = 'E:/GitREPOs/dbsettings.json'
+db_setting_path = 'dbsettings.json'
+folder = os.path.dirname(os.path.abspath(__file__))
+CorridorFile = os.path.join(folder, CorridorFile)
+db_setting_path = os.path.join(folder, db_setting_path)
 # -----------------
 
 def load_dbsetting_file(fpath):
@@ -66,7 +70,7 @@ def query_by_corridor_group(corridor_id, start_date, end_date, output_format='cs
         
     # query data
     DBserver, DBname, UID, PWD, tblname = load_dbsetting_file(db_setting_path)
-    connStr = ( r'DRIVER={SQL Server};SERVER=' +
+    connStr = (r'DRIVER={SQL Server};SERVER=' +
             DBserver + ';DATABASE=' + DBname + ';' +
             'UID=' + UID + ';PWD=' + PWD)
         
@@ -77,8 +81,7 @@ def query_by_corridor_group(corridor_id, start_date, end_date, output_format='cs
     
     query = "SELECT data_datetime, laneDir, sum(VolSum), avg(avg_speed) FROM %s WHERE ACISA IN (%s) " % (tblname, corr_acisas) \
             + "AND data_datetime between \'%s\' and \'%s\' GROUP BY laneDir, data_datetime ORDER BY laneDir, data_datetime ASC" % (start_date, end_date)        
-#    print query
-    
+
     dbCursor = conn.cursor()
     dbCursor.execute(query)
     
@@ -120,5 +123,64 @@ def query_by_corridor_group(corridor_id, start_date, end_date, output_format='cs
         outtext = '\n'.join(t for t in text)
         return outtext
 
+
+def query_by_acisa(acisa, start_date, end_date, output_format='csv'):
+    # query data
+    DBserver, DBname, UID, PWD, tblname = load_dbsetting_file(db_setting_path)
+    connStr = ( r'DRIVER={SQL Server};SERVER=' +
+            DBserver + ';DATABASE=' + DBname + ';' +
+            'UID=' + UID + ';PWD=' + PWD)
+
+    conn = pyodbc.connect(connStr)
+
+#    query = "SELECT data_datetime, ACISA, laneDir, VolSum, avg_speed FROM %s WHERE ACISA IN (%s) " % (tblname, corr_acisas) \
+#            + "AND data_datetime between \'%s\' and \'%s\' ORDER BY ACISA, data_datetime ASC" % (start_date, end_date)
+
+    query = "SELECT data_datetime, laneDir, sum(VolSum), avg(avg_speed) FROM %s WHERE ACISA IN (%s) " % (tblname, acisa) \
+            + "AND data_datetime between \'%s\' and \'%s\' GROUP BY laneDir, data_datetime ORDER BY laneDir, data_datetime ASC" % (start_date, end_date)
+#    print query
+
+    dbCursor = conn.cursor()
+    dbCursor.execute(query)
+
+    info = {}
+
+    for row in dbCursor:
+#        (dt, acisa, lanedr, vol, avgspd) = tuple(row[0:6])
+#        print  dt, acisa, lanedr, vol, avgspd
+        (dt, lanedr, vol, avgspd) = tuple(row[0:4])
+        if(not info.has_key(lanedr)):
+            info[lanedr] = []
+        info[lanedr].append({'datetime': dt,'speed': avgspd, 'vol': vol})
+#        print  dt, lanedr, vol, avgspd
+
+    if(output_format == 'xml'):
+        # construct XML
+        root = ET.Element("items")
+        for k, v in info.items():
+            lanedr = ET.SubElement(root, "lanedr")
+            lanedr.set('direction', k.strip())
+            for v0 in v:
+                item = ET.SubElement(lanedr, "item")
+                item.set("datetime", v0['datetime'].strftime("%Y-%m-%d %H:%M:%S"))
+                item.set("speed", str(v0['speed']))
+                item.set("vol", str(v0['vol']))
+
+        tree = ET.ElementTree(root)
+    #    tree.write("test.xml") # write to a temporary file
+        return ET.dump(tree) # return an XML string
+
+    elif(output_format == 'csv'):
+        # construct csv
+        text = ["week_year,day_time,speed,vol,lanedr"]
+        for k, v in info.items():
+            kk = k.strip()
+            for v0 in v:
+                wday = datetime.datetime(1900,1,7, v0['datetime'].hour, v0['datetime'].minute, v0['datetime'].second) + datetime.timedelta( (v0['datetime'].weekday() + 1) % 7  ,0)
+                text.append("%s,%s,%s,%s,%s" % (v0['datetime'].strftime("%Y-%m-%d"), wday.strftime("%Y-%m-%d %H:%M:%S"), str(v0['speed']), str(v0['vol']), kk))
+        outtext = '\n'.join(t for t in text)
+        return outtext
+
 # testing command
-print query_by_corridor_group('1', '2013-10-01', '2013-10-31')
+#print query_by_corridor_group('1', '2013-10-01', '2013-10-31')
+# print query_by_acisa('2135', '2013-10-01', '2013-10-31')
